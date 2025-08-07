1fa4b02ad0f1268df5069e1d43c7c54d2db8fd87
4snt <murilo.escobedo@protonmail.com>
2025-08-07 08:39:31 -0300

    fix(env): correct env aderess for database

--- oficina/src/main/resources/schema.sql
+++ oficina/src/main/resources/schema.sql
@@ -9,7 +9,8 @@ CREATE TABLE IF NOT EXISTS funcionarios (
     cpf VARCHAR(11) UNIQUE NOT NULL,
     nome VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    cargo VARCHAR(50) NOT NULL
+    cargo VARCHAR(50) NOT NULL,
+    email VARCHAR(255)
 );
 
 -- Exemplo: adicione aqui a criação das demais tabelas necessárias
