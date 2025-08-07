357baaf0ae63cf6392cac9bf82d8446577596866
4snt <murilo.escobedo@protonmail.com>
2025-08-07 07:47:55 -0300

    feat(swagger): add swagger to the api

---
+++ oficina/src/main/java/miguel/projetos/oficina/Config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package miguel.projetos.oficina.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Oficina API")
+                        .version("1.0")
+                        .description("Documentação interativa da API Oficina usando Swagger UI"));
+    }
+}
