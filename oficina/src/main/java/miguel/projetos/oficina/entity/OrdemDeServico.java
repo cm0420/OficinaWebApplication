4b7c0ce8caec5a34df32aac749c34365b50c521c
4snt <murilo.escobedo@protonmail.com>
2025-08-20 11:02:14 -0300

    atualização dos dto

--- oficina/src/main/java/miguel/projetos/oficina/entity/OrdemDeServico.java
+++ oficina/src/main/java/miguel/projetos/oficina/entity/OrdemDeServico.java
@@ -1,17 +1,29 @@
 package miguel.projetos.oficina.entity;
-import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import java.time.LocalDateTime;
-import java.util.List;
+
 @Entity
 @Table(name = "ordens_de_servico")
 @Data
 @NoArgsConstructor
 public class OrdemDeServico {
+
     @Id
     private String numero_os;
 
@@ -26,24 +38,41 @@ public class OrdemDeServico {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_cliente", nullable = false)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
+    @ToString.Exclude @EqualsAndHashCode.Exclude
     private Cliente cliente;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_carro", nullable = false)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
+    @ToString.Exclude @EqualsAndHashCode.Exclude
     private Carro carro;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_mecanico", nullable = false)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
+    @ToString.Exclude @EqualsAndHashCode.Exclude
     private Funcionario mecanico;
 
-    @OneToMany(mappedBy = "ordemDeServico", cascade = CascadeType.ALL, orphanRemoval = true)
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
-    private List<PecaUtilizada> pecasUtilizadas;
+    @OneToMany(mappedBy = "ordemDeServico",
+               cascade = CascadeType.ALL,
+               orphanRemoval = true)
+    @ToString.Exclude @EqualsAndHashCode.Exclude
+    private List<PecaUtilizada> pecasUtilizadas = new ArrayList<>(); // ← nunca nula
+
+    // Getter defensivo (sobrepõe o do Lombok)
+    public List<PecaUtilizada> getPecasUtilizadas() {
+        if (pecasUtilizadas == null) {
+            pecasUtilizadas = new ArrayList<>();
+        }
+        return pecasUtilizadas;
+    }
+
+    // Conveniências para manter os dois lados do relacionamento em ordem
+    public void addPeca(PecaUtilizada p) {
+        getPecasUtilizadas().add(p);
+        p.setOrdemDeServico(this);
+    }
+
+    public void removePeca(PecaUtilizada p) {
+        getPecasUtilizadas().remove(p);
+        p.setOrdemDeServico(null);
+    }
 }
